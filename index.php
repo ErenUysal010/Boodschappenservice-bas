79164681e59e719d601d5655832978456c52bc20
ErenUysal010 <95631312+ErenUysal010@users.noreply.github.com>
2023-06-22 23:24:09 +0200

    update

--- index.php
+++ index.php
@@ -9,9 +9,44 @@
 </head>
 
 <body>
-<?php include 'navbar.php'; ?>
+	<?php include 'navbar.php'; ?>
 
 	<h1>Bas opdracht</h1>
+	<h2>Homepagina</h2><br>
+	<h3>
+		Welkom op de homepage van jouw nieuwe website, die speciaal is ontworpen om gebruikt te worden door BAS.
+		Deze website zal dienen als een krachtig hulpmiddel voor het beheren van klanten, artikelen, verkooporders, inkooporders en leveranciers.
+	</h3>
+	<br>
+
+	<h4>Klanten:</h4>
+	<p>
+		Op deze website kun je klanten toevoegen en bekijken. Het klantbeheersysteem stelt je in staat om belangrijke gegevens van je klanten op te slaan, zoals hun naam, contactgegevens, verzendadres en betalingsinformatie. Met deze functionaliteit kun je gemakkelijk nieuwe klanten toevoegen, bestaande klantgegevens bijwerken en snel informatie over je klanten opvragen wanneer dat nodig is.
+	</p>
+
+	<h4>Artikelen:</h4>
+	<p>
+		Deze sectie stelt je in staat om een overzicht te krijgen van de artikelen die je aanbiedt. Je kunt nieuwe artikelen toevoegen en bestaande artikelen bewerken. Voor elk artikel kun je details opgeven zoals de naam, beschrijving, prijs, voorraadniveau en andere relevante informatie. Dit helpt je bij het bijhouden van je inventaris en het gemakkelijk beheren van de beschikbaarheid van artikelen.
+	</p>
+
+	<h4>Verkooporders:</h4>
+	<p>
+		Met deze functionaliteit kun je verkooporders beheren. Je kunt nieuwe verkooporders aanmaken, bestaande orders bekijken en bijwerken. Voor elke verkooporder kun je informatie zoals klantgegevens, bestelde artikelen, aantallen en totale kosten opgeven. Dit stelt je in staat om overzichtelijk alle lopende bestellingen te beheren en zorgt voor een gestroomlijnd proces voor het verwerken van bestellingen.
+	</p>
+
+	<h4>Inkooporders:</h4>
+	<p>
+		Deze sectie stelt je in staat om inkooporders te beheren. Je kunt nieuwe inkooporders maken en bestaande orders bekijken en bijwerken. Voor elke inkooporder kun je informatie zoals leveranciersgegevens, bestelde artikelen, aantallen en totale kosten specificeren. Dit helpt je bij het efficiënt inkopen van de benodigde materialen en zorgt voor een georganiseerde inkoopworkflow.
+	</p>
+
+	<h4>Leveranciers:</h4>
+	<p>
+		Hier kun je belangrijke informatie over je leveranciers opslaan en beheren. Je kunt nieuwe leveranciers toevoegen en bestaande leveranciers bewerken. Voor elke leverancier kun je gegevens invoeren zoals bedrijfsnaam, contactgegevens, adres en andere relevante informatie. Met deze functie kun je gemakkelijk contact houden met je leveranciers en hun gegevens snel raadplegen wanneer dat nodig is.
+	</p>
+
+	<h3>
+		Deze website is ontworpen om je te helpen bij het efficiënt beheren van je bedrijfsprocessen met betrekking tot klanten, artikelen, verkooporders, inkooporders en leveranciers. Het biedt een georganiseerde en gestroomlijnde interface waarmee je gemakkelijk informatie kunt invoeren, bijwerken en opvragen. We hopen dat deze website je bedrijfsactiviteiten zal verbeteren en je tijd en moeite zal besparen bij het beheren van deze essentiële aspecten van je onderneming.
+	</h3>
 
 </body>
 
